fe1e36aa18e5d73af527f8476e3a9238400f4598
Kush Behl <behlkush@gmail.com>
2022-12-01 13:18:43 -0500

    Improved version with comments

--- day1_challenge1.py
+++ day1_challenge1.py
@@ -1,22 +1,21 @@
-
-
-def calculate_sum_top_3_method1(file_handle):
+def calculate_sum_top_3(file_handle):
     currSum = 0
     list_of_all_summs = []
 
     for line in file_handle:
+        #The elves items are separated with an empty line
         if (line!="\n"):
             currSum += int(line)
-            
         else:
+            #Add the sums of each items belonging to that elf
             list_of_all_summs.append(currSum)
             currSum = 0
 
-
+    #Reverse = true can be used as well but python gives us a way to do backward indexing
     list_of_all_summs.sort()
     finalSum = list_of_all_summs[-1] + list_of_all_summs[-2] + list_of_all_summs[-3]
-    print(finalSum)
+    return(finalSum)
 
-
+# can use with open as well but i want to keep the code to a beginner level
 f = open("8462.txt", "r")
-calculate_sum_top_3_method1(f)
+print(calculate_sum_top_3(f))
